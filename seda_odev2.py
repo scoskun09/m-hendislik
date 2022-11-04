35d29374e3ca9c5ac04de803b1061fb04987e345
scoskun09 <101892196+scoskun09@users.noreply.github.com>
2022-11-04 21:27:01 +0300

    Add files via upload

---
+++ seda_odev2.py
@@ -0,0 +1,593 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print(np.ones((3,3)))
+
+
+# In[2]:
+
+
+import numpy as np
+print(np.ones((3,3)),float)
+
+
+# In[3]:
+
+
+import numpy as np
+print(np.ones((3,3)),float)
+
+
+# In[5]:
+
+
+import numpy as np.Float32
+print(np.ones((3,3)))
+
+
+# In[6]:
+
+
+import numpy as np
+print(np.Float32.ones((3,3)))
+
+
+# In[7]:
+
+
+import numpy as np
+print(np.ones.float((10,10)))
+
+
+# In[8]:
+
+
+import numpy as np.float
+print(np.ones((10,10)))
+
+
+# In[12]:
+
+
+import numpy as np
+print(np.ones((10,10)))
+
+
+# In[15]:
+
+
+import numpy as np
+dtype=np.float32
+print(np.ones((10,10)))
+
+
+# In[21]:
+
+
+import numpy as np
+dtype=np.int64
+numpy_array = np.array([9, 9, 9, 9, 9, 9, 9, 9, 9, 9],[9, 9, 9, 9, 9, 9, 9, 9, 9, 9],[9, 9, 9, 9, 9, 9, 9, 9, 9, 9],[9, 9, 9, 9, 9, 9, 9, 9, 9, 9]
+    ,[9, 9, 9, 9, 9, 9, 9, 9, 9, 9],[9, 9, 9, 9, 9, 9, 9, 9, 9, 9],[9, 9, 9, 9, 9, 9, 9, 9, 9, 9],[9, 9, 9, 9, 9, 9, 9, 9, 9, 9]
+    ,[9, 9, 9, 9, 9, 9, 9, 9, 9, 9],[9, 9, 9, 9, 9, 9, 9, 9, 9, 9])
+
+
+# In[22]:
+
+
+import numpy as np
+b = np.arange(5, 25, 3) # başlangıç, bitiş, adım
+b
+
+
+# In[23]:
+
+
+c = np.linspace(-1, 1, 30) # başlangıç, bitiş, sayı noktaları (num-points) 
+c
+
+
+# In[3]:
+
+
+c.dot(c)
+
+
+# In[5]:
+
+
+x = np.array([np.arange(0,30), np.arange(0,30),np.arange(0,30), np.arange(0,30),np.arange(0,30)])
+x
+
+
+# In[6]:
+
+
+import numpy as np
+x = np.array([np.arange(0,30), np.arange(0,30),np.arange(0,30), np.arange(0,30),np.arange(0,30)])
+x
+
+
+# In[8]:
+
+
+import random
+matris[5][6]
+for i in range 5
+    for j in range 6
+     matrsi[i][j]=random.randint(0,30)
+matris
+
+
+# In[ ]:
+
+
+
+
+
+# In[9]:
+
+
+import random
+matris[5][6]
+for i in range 5:
+    for j in range 6:
+        matrsi[i][j]=random.randint(0,30)
+matris
+
+
+# In[10]:
+
+
+import random
+matris[5][6]
+for i in range 5:
+ for j in range 6:
+  matrsi[i][j]=random.randint(0,30)
+matris
+
+
+# In[11]:
+
+
+import random
+import np
+matris[5][6]
+for i in range(1,5):
+ for j in range(1,6):
+  matrsi[i][j]=random.randint(0,30)
+matris
+
+
+# In[12]:
+
+
+import random
+import numpy as np
+m=Matris[5][6]
+for i in range(1,5):
+ for j in range(1,6):
+  m[i][j]=random.randint(0,30)
+m
+
+
+# In[13]:
+
+
+import random
+import numpy as np
+m=Matris[5][6]
+for i in range(1,5):
+ for j in range(1,6):
+  m[i][j]=random.randint(0,30)
+m
+
+
+# In[15]:
+
+
+m=[[9,9,9,9,9,9,9,9,9,9,9],
+  [9,9,9,9,9,9,9,9,9,9,9],
+  [9,9,9,9,9,9,9,9,9,9,9],
+  [9,9,9,9,9,9,9,9,9,9,9],
+  [9,9,9,9,9,9,9,9,9,9,9],
+  [9,9,9,9,9,9,9,9,9,9,9],
+  [9,9,9,9,9,9,9,9,9,9,9],
+  [9,9,9,9,9,9,9,9,9,9,9],
+  [9,9,9,9,9,9,9,9,9,9,9],
+  [9,9,9,9,9,9,9,9,9,9,9],
+  ]
+
+
+# In[16]:
+
+
+m=[[9,9,9,9,9,9,9,9,9,9,9],
+  [9,9,9,9,9,9,9,9,9,9,9],
+  [9,9,9,9,9,9,9,9,9,9,9],
+  [9,9,9,9,9,9,9,9,9,9,9],
+  [9,9,9,9,9,9,9,9,9,9,9],
+  [9,9,9,9,9,9,9,9,9,9,9],
+  [9,9,9,9,9,9,9,9,9,9,9],
+  [9,9,9,9,9,9,9,9,9,9,9],
+  [9,9,9,9,9,9,9,9,9,9,9],
+  [9,9,9,9,9,9,9,9,9,9,9],
+  ]
+m
+
+
+# In[17]:
+
+
+import random
+import numpy as np
+m=[[9,9,9,9,9,9],
+   [9,9,9,9,9,9],
+   [9,9,9,9,9,9],
+   [9,9,9,9,9,9],
+   [9,9,9,9,9,9],
+   [9,9,9,9,9,9],
+  ]  
+for i in range(1,5):
+ for j in range(1,6):
+  m[i][j]=random.randint(0,30)
+m
+
+
+# In[18]:
+
+
+import numpy as np
+b=np array[[1,2,3,4,5,6]]
+b = b.reshape((2, 3))
+b
+
+
+# In[19]:
+
+
+import numpy as np
+b=np array[[1,2,3,4,5,6]]
+b = b.reshape((2, 3))
+b
+
+
+# In[20]:
+
+
+import numpy as np
+b=np.array([1,2,3,4,5,6])
+b = b.reshape((2, 3))
+b
+
+
+# In[21]:
+
+
+import numpy as np
+a = np.array([[2,4,6]])
+b = np.array([[7,8,9]])
+c = np.array([[3,5,7]])
+d = np.array([[7,4,6]])
+np.vstack((a,b,c,d))
+
+
+# In[22]:
+
+
+import numpy as np
+a = np.array([[2,4,6]])
+b = np.array([[7,8,9]])
+c = np.array([[3,5,7]])
+d = np.array([[7,4,6]])
+np.vstack((a,b,c,d),axis=0)
+
+
+# In[24]:
+
+
+import numpy as np
+a = np.array([[2,4,6], [4,3,2]])
+b = np.array([[7,8,9], [4,3,2]])
+np.vstack((a,b))
+np.hstack((a,b))
+
+
+# In[26]:
+
+
+import numpy as np
+a = np.array([[2,4,6]])
+b = np.array([[7,8,9], [4,3,2]])
+np.vstack((a,b))
+
+
+# In[28]:
+
+
+
+
+
+# In[30]:
+
+
+import numpy as np
+a = np.array([[2,4,6],[4,3,2]])
+b = np.array([[7,8,9], [4,3,2]])
+np.hstack((a,b))
+
+
+# In[31]:
+
+
+a="1,2,3,4,5"
+a.split(",")
+a
+
+
+# In[32]:
+
+
+a="1 , 2 , 3 , 4 , 5"
+a.split(",")
+
+
+# In[34]:
+
+
+import numpy as np
+np.random.seed(3)
+a = np.random.randint(0, 21,5)
+a
+
+
+# In[35]:
+
+
+import numpy as np
+np.random.seed(3)
+a = np.random.randint(0, 21,5)
+a.sort()
+
+
+# In[36]:
+
+
+import numpy as np
+np.random.seed(3)
+a = np.random.randint(0, 21,5)
+a.sort
+
+
+# In[37]:
+
+
+import numpy as np
+np.random.seed(3)
+a = np.random.randint(0, 21,5)
+a
+
+
+# In[38]:
+
+
+array.sort()
+
+
+# In[39]:
+
+
+import numpy as np
+np.random.seed(3)
+a = np.random.randint(0, 21,5)
+a.sort()
+
+
+# In[40]:
+
+
+import numpy as np
+np.random.seed(3)
+a = np.random.randint(0, 21,5)
+
+
+# In[41]:
+
+
+a.sort()
+
+
+# In[42]:
+
+
+import numpy as np
+np.random.seed(3)
+a = np.random.randint(0, 21,5)
+a.Sort()
+
+
+# In[43]:
+
+
+import numpy as np
+np.random.seed(3)
+a = np.random.randint(0, 21,5)
+np.sort(a)
+
+
+# In[47]:
+
+
+import numpy as np
+a = np.random.randint(0, 1, 50)
+np.mean(a) #ortalama
+np.median(a) #median
+np.std(a) #standart sapma
+np.var(a) #varyans
+np.max(a) #maximum
+np.min(a) #minimum
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[48]:
+
+
+import random
+import numpy as np
+m=[[9,9,9,9,9,9,9], 
+   [9,9,9,9,9,9,9],
+   [9,9,9,9,9,9,9],
+   [9,9,9,9,9,9,9],
+  ]  
+for i in range(1,4):
+ for j in range(1,7):
+  m[i][j]=random.randint(0,10)
+m
+
+
+# In[51]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+movies = pd.read_excel('veri1.xls', sheet_name=None)
+movies
+
+
+# In[52]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+movies = pd.read_excel('veri1.xls', sheet_name=None)
+movies
+
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+movies = pd.read_excel('veri1.xls', sheet_name=None)
+movies
+
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+sal = pd.read_csv("./veri.csv")
+sal.head()
+
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+sal = pd.read_csv("./veri.csv")
+ax = sal.Year.hist(figsize=(8,8), grid=False)
+ax.set_xlabel("Year", fontsize=15)
+
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+sal = pd.read_csv("./veri.csv")
+ax = sal.Year.hist(figsize=(8,8), grid=False)
+ax.set_xlabel("Year", fontsize=15)
+
+
+# In[6]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+sal = pd.read_csv("./veri.csv")
+sal["Medal"].value_counts().head(30)-
+
+
+# In[13]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+sal = pd.read_csv("./veri.csv")
+sal['Medal'].value_counts().head(30)
+sal['Medal'].value_counts().plot(kind='bar', figsize=(8,8))
+
+
+# In[14]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+sal = pd.read_csv("./veri.csv")
+ax = sal.plot.scatter(x="Year", y="Sex", fontsize=15, figsize=(8,8))
+ax.set_xlabel("Year", fontsize=15)
+ax.set_ylabel("Sex", fontsize=15)
+
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+sal = pd.read_csv("./adult.csv")
+sal.head()
+
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+sal = pd.read_csv("./adult.csv")
+sal.head()
+
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+sal = pd.read_csv("./adult.csv")
+sal.head()
+
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
